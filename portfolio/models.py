5e84955a2af645120652fa3c764136fd8ace5237
jimmy <stjimmytzuc@gmail.com>
2019-12-23 23:11:40 -0600

    Pagina dinamizado

--- portfolio/models.py
+++ portfolio/models.py
@@ -5,8 +5,10 @@ class Project(models.Model):
     title = models.CharField(max_length=200, verbose_name="Titulo")
     description = models.TextField(verbose_name="Descripcion")
     image = models.ImageField(verbose_name="imagen",upload_to="projects")
+    link = models.URLField(null=True,blank=True, verbose_name="url")
     created = models.DateTimeField(auto_now_add=True,verbose_name="Fecha de creacion")
     updated = models.DateTimeField(auto_now=True, verbose_name="fecha de edicion")
+    
 
     class Meta:
         verbose_name = "proyecto"
