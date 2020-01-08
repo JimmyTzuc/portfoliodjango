80322b94acbf0e7390de8a233bd7f9d0c3b49aff
jimmy <stjimmytzuc@gmail.com>
2020-01-07 19:08:09 -0600

    mudado a postgress

--- webpersonal/settings.py
+++ webpersonal/settings.py
@@ -76,9 +76,18 @@ WSGI_APPLICATION = 'webpersonal.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'portfolio',
+        'HOST': 'localhost',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'PORT': 5432,
+        
     }
 }
 
@@ -125,4 +134,5 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 
+# user:jimmy
 # pass:pa55word 
